32e1fc3981afb6f38717463e8da7e0bbedaf24d9
Jacob <vjs@ornl.gov>
2024-10-24 14:30:46 -0400

    add numpy version of test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,10 @@
 import numpy as np
 import pytest
 
-from examples.h5_handling import get_h5py_data
-
 
 @pytest.fixture(scope="session")
 def flourescent_data():
-    data = get_h5py_data(
-        "./examples/data/fluorescentlights.h5", "/fluorescentlights/1/on", 0, 2_000_000
-    )
+    data = np.load("./tests/data/flourescent_data_1_on.npy")
 
     return data
 
